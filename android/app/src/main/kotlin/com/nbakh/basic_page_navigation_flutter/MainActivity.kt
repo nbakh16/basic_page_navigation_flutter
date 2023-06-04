171c2f2d2a3a0b0f44c040526772cc52635852c1
Nabil160 <nbakhunjee97@gmail.com>
2023-06-04 23:25:56 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/nbakh/basic_page_navigation_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nbakh.basic_page_navigation_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: const HomeActivity()
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("App Bar"),),
+      body: Center(child: Text("Body")),
+    );
+  }
+
+}
